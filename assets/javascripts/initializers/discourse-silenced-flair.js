e1d4ab8bb0b95e368edb40f30a66b348c72fae4a
chapoi <101828855+chapoi@users.noreply.github.com>
2023-05-25 14:30:22 +0000

    init

---
+++ assets/javascripts/initializers/discourse-silenced-flair.js
@@ -0,0 +1,34 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { iconNode } from "discourse-common/lib/icon-library";
+
+export default {
+  name: "discourse-silenced-flair",
+  initialize() {
+    withPluginApi("0.1", (api) => {
+      // api.includePostAttributes("avatar-flair-silenced"); //no clue what this does or how to use
+
+      const h = require("virtual-dom").h;
+
+      api.createWidget("avatar-flair-silenced", {
+        tagName: "div.avatar-flair-silenced",
+
+        html() {
+          return h(
+            "span",
+            iconNode("microphone-slash", {
+              class: "avatar-flair-silenced__icon",
+              title: "Silenced",
+            })
+          );
+        },
+      });
+
+      api.decorateWidget("post-avatar:after", (helper) => {
+        const user = helper.getModel();
+        if (user && user.get("silenced")) {
+          return helper.widget.attach("avatar-flair-silenced");
+        }
+      });
+    });
+  },
+};
