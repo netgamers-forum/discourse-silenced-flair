c8e6d6ddb34592c857c35a1cecdaab34ae2c3fba
chapoi <charlie@discourse.org>
2023-05-29 13:03:04 +0700

    Smaller icon

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # about: Adds a flair to silenced accounts, only visible to admins
 # version: 0.0.1
 # authors: chapoi
-# url: TODO
+# url: https://github.com/chapoi/discourse-silenced-flair
 # required_version: 2.7.0
 
 enabled_site_setting :discourse_silenced_flair_enabled
@@ -15,4 +15,3 @@ after_initialize do
   add_to_serializer(:post, :silenced) { object.user.silenced? }
   add_to_serializer(:user_card, :silenced) { object.silenced? }
 end
-
--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -1,24 +1,17 @@
-require 'rails_helper'
+
+# frozen_string_literal: true
+
+require "rails_helper"
 
 describe ::DiscourseSilencedFlair do
-
-  # im sure I need to make a user thats silenced somehow
-  let(:user) { Fabricate.build(:user)}
+  let(:user) { Fabricate.build(:user) }
   let!(:silenced) { Fabricate(:silenced, user_id: user.id) }
 
-  before do
-    SiteSetting.discourse_silenced_flair_enabled = true
-  end
-
-
-  it "adds silenced attribute to post serializer" do
-   
-  end
+  before { SiteSetting.discourse_silenced_flair_enabled = true }
 
   it "adds silenced attribute to user card serializer" do
     serializer = UserCardSerializer.new(user)
     expect(serializer).to respond_to(:silenced)
     expect(serializer.silenced).to eq(true)
   end
-
 end
